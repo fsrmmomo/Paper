7b119508a3f6948347dfcd965ec25a4ced4b5424
fsrmmomo <1906994189@qq.com>
2022-02-21 17:16:34 +0800

    count

--- src/data_analysis/count.py
+++ src/data_analysis/count.py
@@ -23,7 +23,7 @@ def collect_statistic_data(file_name, trace_byte_size=13):
             else:
                 stat[t.bin_src_ip()] = stat[t.bin_src_ip()] + 1
 
-            if(x/10000!=0 and x%10000==0):
+            if(x%1000==0):
                 count_array.append(len(stat))
                 stat.clear()
 
@@ -52,9 +52,9 @@ if __name__ == "__main__":
     cnt_file_folder = "../../Data/cnt/"
     # csv_file_folder = sys.argv[2]
     for file in os.listdir(dat_file_folder):
-        if file[-3:] == "dat":
-        # if file == "1.dat":
+        # if file[-3:] == "dat":
+        if file == "1.dat":
             print("Process: ", dat_file_folder + file)
             count_array = collect_statistic_data(dat_file_folder + file)
 
-            save_statistic_data(cnt_file_folder + file[:-4] + ".cnt", count_array)
+            # save_statistic_data(cnt_file_folder + file[:-4] + ".cnt", count_array)
